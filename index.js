067f45ae0be9079a1f4487996eda88d7e1916277
Denisa-Nicole Oprean <denisaoprean@yahoo.com>
2022-09-07 16:43:19 +0200

    Create app functionality

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('test');
+import chalk from 'chalk';
+import randomColor from 'randomcolor';
+
+const color = randomColor({
+  luminosity: process.argv[3],
+  hue: process.argv[2],
+});
+
+const colorfulBox = `#######################################
+#######################################
+#######################################
+#####                             #####
+#####           ${color}           #####
+#####                             #####
+#######################################
+#######################################
+#######################################`;
+
+console.log(chalk.hex(color)(colorfulBox));
